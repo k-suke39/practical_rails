a5615d2884a7fd6a1da88897d0efdfa64fbf2f09
k-suke39 <pz.mozarla@gmail.com>
2023-06-22 22:31:06 +0900

    🔥 only hashの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -61,7 +61,7 @@ class Article < ApplicationRecord
 
   scope :viewable, -> { published.where('published_at < ?', Time.current) }
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
-  scope :by_category, ->(category_id) { where(category_id: category_id) }
+  scope :by_category, ->(category_id) { where(category_id:) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
 
   def build_body(controller)
