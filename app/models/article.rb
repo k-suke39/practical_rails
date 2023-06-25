ca39ec3616ff0b63c9292b6fe52b32a279b3d99b
k-suke39 <pz.mozarla@gmail.com>
2023-06-25 15:46:20 +0900

    ✨  記事ステータスの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -39,7 +39,7 @@ class Article < ApplicationRecord
 
   has_one_attached :eye_catch
 
-  enum state: { draft: 0, published: 1 }
+  enum state: { draft: 0, published: 1, publish_wait: 2 }
 
   validates :slug, slug_format: true, uniqueness: true, length: { maximum: 255 }, allow_blank: true
   validates :title, presence: true, uniqueness: true, length: { maximum: 255 }
@@ -63,6 +63,7 @@ class Article < ApplicationRecord
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
   scope :by_category, ->(category_id) { where(category_id:) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
+  scope :past_published, -> { where('published_at <= ?', Time.current) }
 
   def build_body(controller)
     result = ''
@@ -79,6 +80,27 @@ class Article < ApplicationRecord
                 end
     end
     result
+  end
+
+  def publishable?
+    Time.current >= published_at
+  end
+
+  def message_on_published
+    if published?
+      '記事を公開しました'
+    elsif publish_wait?
+      '記事を公開待ちにしました'
+    end
+  end
+
+  def adjust_state
+    return if draft?
+    self.state = if publishable?
+                   :published
+                 else
+                   :publish_wait
+                 end
   end
 
   def next_article
